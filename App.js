e126411941e82c710ba58f7def9c906daf99ebd2
lovbackan <simon@lovbacka.se>
2023-09-05 10:32:18 +0200

    ts config

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const name = "Adam";
+
+const myFunction = (input) => {
+  return `Hello ${name}`;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={{ color: "red" }}>Hello World!</Text>
+      <TextInput></TextInput>
       <Text>
         Open up App.js to start working on your app! TESTING TESTING Hallå ADAM!
+        Funkar detta fortfarande?
       </Text>
       <StatusBar style="auto" />
     </View>

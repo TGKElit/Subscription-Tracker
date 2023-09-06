51652776b15997fd72f2060e0657ac5889768e6f
lovbackan <simon@lovbacka.se>
2023-09-06 13:43:24 +0200

    testing

---
+++ App.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Register } from "./src/Register/Register";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} />
+        {/* Add other screens/routes here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+// import React, { useState } from "react";
+// import { StatusBar } from "expo-status-bar";
+// import {
+//   SafeAreaView,
+//   View,
+//   Text,
+//   StyleSheet,
+//   TextInput,
+//   Pressable,
+//   Keyboard,
+//   Alert,
+// } from "react-native";
+// import { CTAButton } from "./src/Components/CTAButton/CTAButton";
+
+// export default function App() {
+//   const [name, setName] = useState<string | undefined>();
+//   const [email, setEmail] = useState<string | undefined>();
+//   const [password, setPassword] = useState<string | undefined>();
+//   return (
+//     <Pressable style={styles.contentView} onPress={Keyboard.dismiss}>
+//       <SafeAreaView style={styles.contentView}>
+//         <View style={styles.container}>
+//           <View style={styles.titleContainer}>
+//             <Text style={styles.titleText}>Register</Text>
+//           </View>
+//           <View style={styles.mainContent}>
+//             <TextInput
+//               style={styles.loginTextField}
+//               placeholder="Name"
+//               value={name}
+//               onChangeText={setName}
+//             />
+//             <TextInput
+//               style={styles.loginTextField}
+//               placeholder="Email"
+//               value={email}
+//               onChangeText={setEmail}
+//               inputMode="email"
+//               autoCapitalize="none"
+//             />
+//             <TextInput
+//               style={styles.loginTextField}
+//               placeholder="Password"
+//               value={password}
+//               onChangeText={setPassword}
+//               secureTextEntry
+//             />
+//           </View>
+//           {/* <CTAButton
+//             title="Sign Up"
+//             onPress={registerAndGoToMainFlow}
+//             variant="primary"
+//           />
+//           <CTAButton title="Go Back" onPress={nav.goBack} variant="secondary" /> */}
+//         </View>
+//       </SafeAreaView>
+//     </Pressable>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   contentView: {
+//     flex: 1,
+//     backgroundColor: "white",
+//   },
+//   container: {
+//     flex: 1,
+//     marginHorizontal: 50,
+//     backgroundColor: "white",
+//     paddingTop: 20,
+//   },
+//   titleContainer: {
+//     flex: 1.2,
+//     justifyContent: "center",
+//   },
+//   titleText: {
+//     fontSize: 45,
+//     textAlign: "center",
+//     fontWeight: "200",
+//   },
+//   loginTextField: {
+//     borderBottomWidth: 1,
+//     height: 60,
+//     fontSize: 30,
+//     marginVertical: 10,
+//     fontWeight: "300",
+//   },
+//   mainContent: {
+//     flex: 6,
+//   },
+// });

d7835ec1001f57ba8cede07989c7fef590826f2d
lovbackan <simon@lovbacka.se>
2023-09-13 11:10:05 +0200

    styling landing page

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -12,26 +12,30 @@ const LandingScreen = ({ navigation }) => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        gap: "44px",
-        marginHorizontal: "12px",
+        gap: 44,
+        marginHorizontal: 12,
       }}
     >
-      <View
-        style={{ height: "342px", backgroundColor: "grey", width: "100%" }}
-      ></View>
       <StatusBar style="dark" />
-      <Text style={{ fontFamily: "Inter_600SemiBold", fontSize: 48 }}>
-        tracky.
-      </Text>
-      <Text
-        style={{
-          fontFamily: "Inter_400Regular",
-          fontSize: 16,
-        }}
-      >
-        Lorem ipsum dolor sit amet consectetur. Donec augue elit praesent
-        faucibus quisque malesuada vitae pellentesque aliquam.
-      </Text>
+      <View
+        style={{ height: "40%", backgroundColor: "grey", width: "100%" }}
+      ></View>
+
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontFamily: "Inter_600SemiBold", fontSize: 48 }}>
+          tracky.
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Inter_400Regular",
+            fontSize: 16,
+            textAlign: "center",
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur. Donec augue elit praesent
+          faucibus quisque malesuada vitae pellentesque aliquam.
+        </Text>
+      </View>
 
       <View>
         <CTAButtonBig
@@ -45,11 +49,11 @@ const LandingScreen = ({ navigation }) => {
           onPress={() => navigation.navigate("Register")}
           variant="secondary"
         />
+        <Button
+          title="Go to Settings"
+          onPress={() => navigation.navigate("Settings")}
+        />
       </View>
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate("Settings")}
-      />
     </SafeAreaView>
   );
 };

292b0fb9a5a8bb5389bc8c6db7ed1b646af6389c
TGKElit <tgkelit@gmail.com>
2023-09-20 19:01:26 +0200

    fixed svgs in navbar

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,57 +1,57 @@
-// import React, { FC } from "react";
+import React, { FC } from "react";
 
-// import { Text, Image, StyleSheet, Pressable, ImageSourcePropType } from "react-native";
+import { Text, Image, StyleSheet, Pressable, ImageSourcePropType } from "react-native";
 
-// type CardType = "default" | "settings" | "transparent";
+type CardType = "default" | "settings" | "transparent";
 
-// interface CardProps {
-//     variant: CardType,
-//     icon: ImageSourcePropType,
-//     title: string,
-//     info: string,
-// }
+interface CardProps {
+    variant: CardType,
+    icon: ImageSourcePropType,
+    title: string,
+    info: string,
+}
 
-// export const Card: FC<CardProps> = ({
-//     variant,
-//     icon,
-//     title,
-//     info,
-// }) => {
+export const Card: FC<CardProps> = ({
+    variant,
+    icon,
+    title,
+    info,
+}) => {
 
-//     const containerStyle =
-//         variant === "default" ? styles.defaultContainer : styles.settingsContainer;
+    const containerStyle =
+         variant === "default" ? styles.defaultContainer : styles.settingsContainer;
 
-//     return (
-//         <Pressable style={[containerStyle, styles.container]}>
-//             <Image style={styles.icon} source={icon}/>
-//             <Text>{title}</Text>
-//             <Text>{info}</Text>
-//             <Image style={styles.icon} source={require('/assets/icons/icon=sideway-arrow.svg')} />
-//         </Pressable>
-//     )
-// }
+    return (
+        <Pressable style={[containerStyle, styles.container]}>
+            <Image style={styles.icon} source={icon}/>
+            <Text>{title}</Text>
+            <Text>{info}</Text>
+            <Image style={styles.icon} source={require('/assets/icons/icon=sideway-arrow.svg')} />
+        </Pressable>
+    )
+}
 
-// const styles = StyleSheet.create({
+const styles = StyleSheet.create({
 
-//   icon: {
-//     width: 32,
-//     height: 32,
-//   },
+  icon: {
+    width: 32,
+    height: 32,
+  },
 
-//   container: {
-//     display: "flex",
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//   },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
 
-//   defaultContainer: {
-//     backgroundColor: "gray",
-//     borderRadius: 12,
-//   },
+  defaultContainer: {
+    backgroundColor: "gray",
+    borderRadius: 12,
+  },
 
-//   settingsContainer: {
-//     backgroundColor: "white",
-//     borderBottomColor: "black",
-//     borderBottomWidth: 2,
-//   },
-// });
+  settingsContainer: {
+    backgroundColor: "white",
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+  },
+});
